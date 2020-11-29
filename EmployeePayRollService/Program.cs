61f3a082c9333ab7fd50aeee2edb967d11ed471b
Shreya0207 <shanu.shreya270@gmail.com>
2020-11-29 18:59:21 +0530

    [Shreya]Add: Estabilishing connection with sql server and retrieving data from database

---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeePayRollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee pay roll.");
+            EmployeeRepo repo = new EmployeeRepo();
+            repo.GetAllEmployee();
+            //EmployeeModel employee = new EmployeeModel();
+            //employee.EmployeeName = "Shreya";
+            //employee.Department = "SE";
+            //employee.PhoneNumber = "9876543210";
+            //employee.Address = "Bhilai";
+            //employee.Gender = 'F';
+            //employee.BasicPay = 40000.00M;
+            //employee.Deductions = 1100.00;
+            //employee.StartDate = employee.StartDate = Convert.ToDateTime("2020-11-05");
+
+            //if (repo.AddEmployee(employee))
+            //    Console.WriteLine("Records added successfully");
+        }
+    }
+}
